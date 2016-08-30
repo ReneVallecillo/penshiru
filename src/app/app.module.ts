c7cb9b7eefc800b370a64901601a1c83a2410f2b
Rene Vallecillo <reneval@gmail.com>
2016-08-30 17:28:11 -0600

    integrating with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Material 2 
+import { MdCoreModule } from '@angular2-material/core'
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdCheckboxModule } from '@angular2-material/checkbox'
+import { MdTooltipModule } from '@angular2-material/tooltip';
+import { MdSliderModule } from '@angular2-material/slider';
+import { MdToolbar} from '@angular2-material/toolbar'
+import 'hammerjs'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +23,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdCoreModule, MdCardModule, MdButtonModule, MdRadioModule,
+    MdCheckboxModule, MdTooltipModule, MdSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
