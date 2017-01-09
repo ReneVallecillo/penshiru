0d1925bb6d0ea8126482943386980c50d1575870
Rene Vallecillo <reneval@gmail.com>
2017-01-09 00:03:41 -0600

    returning a Law instance instead of object

--- src/app/law/shared/law.service.ts
+++ src/app/law/shared/law.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions} from '@angular/http';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Http,
+  Response,
+  Headers,
+  RequestOptions
+} from '@angular/http';
 
-import {Law, LawTmp} from '../../shared/models/';;
-import {Observable} from 'rxjs/Observable';
+import {
+  Law,
+  LawTmp
+} from '../../shared/models/';
+import {
+  Observable
+} from 'rxjs/Observable';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -10,42 +22,47 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class LawService {
 
-  constructor(private http: Http) {}
-  
+  constructor(private http: Http) { }
+
   private lawurl = 'http://localhost:8080/api/laws';
   private lawtmpurl = 'http://localhost:8080/api/tmp/laws';
-  
-  getLaws() : Observable<Law[]>{
+
+  getLaws(): Observable<Law[]> {
     return this.http.get(this.lawurl)
       .map(this.extractDataT)
       .catch(this.handleError);
   }
-  
-  getTmpLaws() : Observable<LawTmp[]>{
+
+  getTmpLaws(): Observable<LawTmp[]> {
     return this.http.get(this.lawtmpurl)
-    .map(this.extractDataT)
-    .catch(this.handleError)
+      .map(this.extractDataT)
+      .catch(this.handleError);
   }
 
-  getTmpLaw(uri:string) : Observable<Law>{
-    return this.http.get(this.lawtmpurl+'/'+uri)
-    .map(this.extractData)
-    .catch(this.handleError)
+  getTmpLaw(uri: string): Observable<Law> {
+    return this.http.get(this.lawtmpurl + '/' + uri)
+      .map(this.extractLaw)
+      .catch(this.handleError);
   }
-  
-  private extractData(res: Response){
+
+  private extractData(res: Response) {
     let body = res.json();
-    console.log(body)
     return body || {};
   }
 
-  //fix later
-   private extractDataT(res: Response){
+  private extractLaw(res: Response) {
+    let body = res.json();
+    let law = Law.fromJSON(body.data);
+    return law || {};
+  }
+
+  // fix later
+  private extractDataT(res: Response) {
     let body = res.json();
     return body.data || [];
   }
-  
-  private handleError (error: any) {
+
+  private handleError(error: any) {
     // In a real world app, we might use a remote logging infrastructure
     // We'd also dig deeper into the error to get a better message
     let errMsg = (error.message) ? error.message :
--- src/app/shared/models/law.model.ts
+++ src/app/shared/models/law.model.ts
@@ -1,29 +1,53 @@
-import {Title} from './title.model';
+import { Title } from './title.model';
 
-export class Law{
-    
+export class Law {
+
     constructor(
-        public id: number,
+        id: number,
         public name: string,
         public titles: Title[],
         public approvalDate: Date,
         public publishData: Date,
         public journal: string,
-        public intro: string
-    ){}
-    
-    static fromJson(json:string){
-        var data = JSON.parse(json);
-        return new Law(
-            data.id, 
-            data.name, 
-            data.titles, 
-            data.approvalDate,
-            data.publishData,
-            data.journal,
-            data.intro)
+        public intro: string,
+    ) { }
+
+    // tslint:disable-next-line:member-ordering
+    static fromJSON(json: LawJSON | string): Law {
+        if (typeof json === 'string') {
+            return JSON.parse(json, Law.reviver);
+        } else {
+            let law = Object.create(Law.prototype);
+            return Object.assign(law, json, {});
+        }
     }
 
-    
-    
+    // reviver can be passed as the second parameter to JSON.parse
+    // to automatically call User.fromJSON on the resulting value.
+    static reviver(key: string, value: any): any {
+        return key === '' ? Law.fromJSON(value) : value;
+    }
+
+    // static fromJson(json: string) {
+    //     var data = JSON.parse(json);
+    //     return new Law(
+    //         data.id,
+    //         data.name,
+    //         data.titles,
+    //         data.approvalDate,
+    //         data.publishData,
+    //         data.journal,
+    //         data.intro)
+    // }
+
+}
+
+interface LawJSON {
+    id: number;
+    name: string;
+    titles: Title[];
+    approvalDate: Date;
+    publishData: Date;
+    journal: string;
+    intro: string;
 }
