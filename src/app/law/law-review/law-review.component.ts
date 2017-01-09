067f33100edb4c9c3ce6258fe747c1370ec148c5
Rene Vallecillo <reneval@gmail.com>
2017-01-09 00:02:19 -0600

    moved to scss

--- src/app/law/law-review/law-review.component.ts
+++ src/app/law/law-review/law-review.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 
-import {LawService} from '../shared/law.service';
+import { LawService } from '../shared/law.service';
 
 
 @Component({
   selector: 'app-law-review',
   templateUrl: './law-review.component.html',
-  styleUrls: ['./law-review.component.css'],
-  providers:[LawService],
+  styleUrls: ['./law-review.component.scss'],
+  providers: [LawService],
 })
 export class LawReviewComponent implements OnInit {
   title = 'Review Law';
-  constructor(  private service:LawService  ) {}
+  constructor(private service: LawService) { }
 
 
   ngOnInit() {
