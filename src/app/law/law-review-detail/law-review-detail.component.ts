5107a83376cf4a9174186fa8d2f33b92546ceee9
Rene Vallecillo <reneval@gmail.com>
2017-01-09 00:04:15 -0600

    adding action buttons on review

--- src/app/law/law-review-detail/law-review-detail.component.ts
+++ src/app/law/law-review-detail/law-review-detail.component.ts
@@ -1,9 +1,24 @@
-import { Component, OnInit,ChangeDetectorRef } from '@angular/core';
-import {Law} from '../../shared/models/law.model';
-import {Router, ActivatedRoute} from '@angular/router';
-import {LawService} from '../shared/law.service';
-import {ReviewService} from '../shared/review.service';
-import {directory} from '../../shared/models/directory';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef
+} from '@angular/core';
+import {
+  Law
+} from '../../shared/models/law.model';
+import {
+  Router,
+  ActivatedRoute
+} from '@angular/router';
+import {
+  LawService
+} from '../shared/law.service';
+import {
+  ReviewService
+} from '../shared/review.service';
+import {
+  directory
+} from '../../shared/models/directory';
 // import {Autosize} from 'angular2-autosize/angular2-autosize';
 
 
@@ -12,52 +27,88 @@ import {directory} from '../../shared/models/directory';
   selector: 'law-review-detail',
   templateUrl: './law-review-detail.component.html',
   styleUrls: ['./law-review-detail.component.scss'],
-  providers:[ReviewService]
+  providers: [ReviewService]
 })
 export class LawReviewDetailComponent implements OnInit {
-  
-  private sub:any;
-  law:Law;
-  history:string [] = [];
-  selectedIndex:number = 0;
+
+  private sub: any;
+  law: Law;
+  history: string[] = [];
+  selectedIndex: number = 0;
   currentLvl: directory;
 
 
   constructor(
-    private router:Router, 
-    private route:ActivatedRoute,
-    private service:LawService,
-    private reviewService:ReviewService) {
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: LawService,
+    private reviewService: ReviewService) {
 
-      this.reviewService.itemSelected$.subscribe(
-        dir =>{
-          this.history.push(`Dir ${dir.name} was selected`)
-          this.currentLvl = dir ;
-        }
-      )
-    }
+    this.reviewService.itemSelected$.subscribe(
+      dir => {
+        this.history.push(`Dir ${dir.name} was selected`);
+        this.currentLvl = dir;
+      }
+    );
+  }
 
-   ngOnInit() {
+  ngOnInit() {
     this.sub = this.route.params.subscribe(
       params => {
         let file = params['file'];
         this.service.getTmpLaw(file).subscribe(
-          law => { this.law = law},
-          error =>{},
-          () => {console.log(this.law)}
-        )
+          law => {
+            this.law = law;
+          },
+          error => { },
+          () => {
+            // console.log(this.law);
+          }
+        );
       }
-    )
+    );
   }
 
-  check(dir:directory){
-        // let newState = !dir.reviewed;
-        dir.reviewed = !dir.reviewed;
-        console.log("change main")
-        // this._changeDetectionRef.detectChanges();
-        //this.checkRecursive(newState);
+  check(dir: directory) {
+    // let newState = !dir.reviewed;
+    dir.reviewed = !dir.reviewed;
+    console.log('change main');
+    // this._changeDetectionRef.detectChanges();
+    // this.checkRecursive(newState);
+  }
+
+  checkAll() {
+    for (let t of this.law.titles) {
+      t.reviewed = true;
+      if (t.chapters.length > 0) {
+        for (let c of t.chapters) {
+          c.reviewed = true;
+          if (c.articles.length > 0) {
+            for (let a of c.articles) {
+              a.reviewed = true;
+            }
+          }
+        }
+      }
     }
+  }
 
-  
-
+  checkIfAll(): boolean {
+    if (this.law != null) {
+      for (let t of this.law.titles) {
+        if (t.reviewed == false) { return false; }
+        if (t.chapters.length > 0) {
+          for (let c of t.chapters) {
+            if (c.reviewed == false) { return false; }
+            if (c.articles.length > 0) {
+              for (let a of c.articles) {
+                if (a.reviewed == false) { return false; }
+              }
+            }
+          }
+        }
+      }
+    }
+    return true;
+  }
 }
--- src/app/shared/models/article.model.ts
+++ src/app/shared/models/article.model.ts
@@ -1,16 +1,15 @@
+export class Article {
 
-export class Article{
-        
     constructor(
-        public id:number, 
+        public id: number,
         public name: string,
         public text: string,
         public chapterID: number,
-        public type: string = 'article'){      
-    }
-    
-    static fromJson(json:string){
-        var data = JSON.parse(json);
-        return new Article(data.id, data.name, data.text, data.chapterID)
+        public type: string = 'article',
+        public reviewed: boolean) { }
+
+    static fromJson(json: string) {
+        // let data = JSON.parse(json);
+        // return new Article(data.id, data.name, data.text, data.chapterID, false);
     }
 }
