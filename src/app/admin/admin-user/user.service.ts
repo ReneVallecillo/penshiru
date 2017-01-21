1552f276bc0b9c2e3d80ce92805aa937655ccf9d
Rene Vallecillo <reneval@gmail.com>
2017-01-20 21:23:34 -0600

    adding user service + tests

---
+++ src/app/admin/admin-user/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { User } from '../../shared/models/user';
+
+@Injectable()
+export class UserService {
+  lastID: number = 0;
+  users: User[] = []
+
+  constructor() { }
+
+  addUser(user: User): UserService {
+    if (!user.id) {
+      user.id = ++this.lastID;
+    }
+    this.users.push(user)
+    return this;
+  }
+
+  deleteUserById(id: number): UserService {
+    this.users = this.users
+      .filter(user => user.id !== id);
+
+    return this;
+  }
+
+  updateUserById(id: number, values: Object = {}): User {
+    let user = this.getUserById(id);
+    if (!user) {
+      return null;
+    }
+    Object.assign(user, values);
+    return user;
+  }
+
+  // Simulate GET /todos
+  getAllUsers(): User[] {
+    return this.users;
+  }
+
+  getUserById(id: number): User {
+    return this.users
+      .filter(user => user.id === id)
+      .pop();
+  }
+
+  // Toggle todo complete
+  toggleUserActive(user: User) {
+    let updatedUser = this.updateUserById(user.id, {
+      active: !user.active
+    });
+    return updatedUser;
+  }
+
+}
