e7cca3c6ba9ea483e2f1b46cd504db0215bd36c7
Rene Vallecillo <reneval@gmail.com>
2017-03-06 17:18:59 -0600

    adding support for laws with book sub division

--- src/app/law/law-review-detail/law-review-detail.component.ts
+++ src/app/law/law-review-detail/law-review-detail.component.ts
@@ -86,13 +86,31 @@ export class LawReviewDetailComponent implements OnInit {
     // this.checkRecursive(newState);
   }
 
+
   checkAll() {
-    for (let t of this.law.titles) {
-      t.reviewed = true;
-      if (t.chapters.length > 0) {
-        for (let c of t.chapters) {
-          c.reviewed = true;
-          if (c.articles != null) {
+    if (this.law.books) {
+      for (let b of this.law.books) {
+        b.reviewed = true;
+        for (let t of b.titles) {
+          t.reviewed = true;
+          if (t.chapters.length > 0) {
+            for (let c of t.chapters) {
+              c.reviewed = true;
+              if (c.articles.length > 0) {
+                for (let a of c.articles) {
+                  a.reviewed = true;
+                }
+              }
+            }
+          }
+        }
+      }
+    } else {
+      for (let t of this.law.titles) {
+        t.reviewed = true;
+        if (t.chapters.length > 0) {
+          for (let c of t.chapters) {
+            c.reviewed = true;
             if (c.articles.length > 0) {
               for (let a of c.articles) {
                 a.reviewed = true;
@@ -105,21 +123,46 @@ export class LawReviewDetailComponent implements OnInit {
   }
 
   checkIfAll(): boolean {
-    // if (this.law != null) {
-    //   for (let t of this.law.titles) {
-    //     if (t.reviewed == false) { return false; }
-    //     if (t.chapters.length > 0) {
-    //       for (let c of t.chapters) {
-    //         if (c.reviewed == false) { return false; }
-    //         if (c.articles.length > 0) {
-    //           for (let a of c.articles) {
-    //             if (a.reviewed == false) { return false; }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
+    if (this.law != null) {
+      if (this.law.books) {
+        for (let b of this.law.books) {
+          if (b.reviewed == false) { return false; }
+          for (let t of b.titles) {
+            if (t.reviewed == false) { return false; }
+            if (t.chapters.length > 0) {
+              for (let c of t.chapters) {
+                if (c.reviewed == false) { return false; }
+                if (c.articles.length > 0) {
+                  for (let a of c.articles) {
+                    if (a.reviewed == false) { return false; }
+                  }
+                }
+              }
+            }
+          }
+        }
+      } else {
+        for (let t of this.law.titles) {
+          if (t.reviewed == false) { return false; }
+          if (t.chapters.length > 0) {
+            for (let c of t.chapters) {
+              if (c.reviewed == false) { return false; }
+              if (c.articles.length > 0) {
+                for (let a of c.articles) {
+                  if (a.reviewed == false) { return false; }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+
+    if (this.law != null) {
+
+    }
     return true;
   }
 
