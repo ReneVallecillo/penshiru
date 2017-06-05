0e28f5f9202cc9cb70f078f12ae67717878d5944
Rene Vallecillo <reneval@gmail.com>
2017-06-05 15:36:00 -0600

    adding missing modules to material module

--- src/app/shared/penshiru-material.module.ts
+++ src/app/shared/penshiru-material.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import {
     MdDialogModule, MdCheckboxModule, MdCardModule, MdInputModule,
     MdButtonModule, MdTabsModule, MdOptionModule, MdAutocompleteModule,
-    MdToolbarModule, MdIconModule
+    MdToolbarModule, MdIconModule, MdSidenavModule, MdListModule
 } from '@angular/material';
 
 
 @NgModule({
     imports: [MdDialogModule, MdCheckboxModule, MdCardModule, MdInputModule,
         MdButtonModule, MdTabsModule, MdOptionModule, MdAutocompleteModule,
-        MdToolbarModule, MdIconModule],
+        MdToolbarModule, MdIconModule, MdSidenavModule, MdListModule],
     exports: [MdDialogModule, MdCheckboxModule, MdCardModule, MdInputModule,
         MdAutocompleteModule, MdButtonModule, MdTabsModule, MdOptionModule,
-        MdToolbarModule, MdIconModule],
+        MdToolbarModule, MdIconModule, MdSidenavModule, MdListModule],
     declarations: [],
     providers: [],
 })
