daa114d31dcbfda833efc985a57525bbafa93344
Rene Vallecillo <reneval@gmail.com>
2017-06-05 15:24:22 -0600

    fixing menu

--- src/app/shared/penshiru-material.module.ts
+++ src/app/shared/penshiru-material.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import {
     MdDialogModule, MdCheckboxModule, MdCardModule, MdInputModule,
-    MdButtonModule, MdTabsModule, MdOptionModule, MdAutocompleteModule
+    MdButtonModule, MdTabsModule, MdOptionModule, MdAutocompleteModule,
+    MdToolbarModule, MdIconModule
 } from '@angular/material';
 
 
 @NgModule({
     imports: [MdDialogModule, MdCheckboxModule, MdCardModule, MdInputModule,
-        MdButtonModule, MdTabsModule, MdOptionModule, MdAutocompleteModule],
+        MdButtonModule, MdTabsModule, MdOptionModule, MdAutocompleteModule,
+        MdToolbarModule, MdIconModule],
     exports: [MdDialogModule, MdCheckboxModule, MdCardModule, MdInputModule,
-        MdAutocompleteModule, MdButtonModule, MdTabsModule, MdOptionModule],
+        MdAutocompleteModule, MdButtonModule, MdTabsModule, MdOptionModule,
+        MdToolbarModule, MdIconModule],
     declarations: [],
     providers: [],
 })
